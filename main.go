d9da858fa80d95031e0ab5ba29a31ca6f9d568fd
mihir karbelkar <karbelkar.mihir@gmail.com>
2024-08-17 21:03:24 +0530

    feat(book dowloand): init

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"libgenscrape/views"
+	"libgenscrape/views/components"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	component := views.Index()
+	component.Render(r.Context(), w)
+}
+
+type SearchPayload struct {
+	Query string `json:"query"`
+}
+
+func GetDomain(urlToParse string) string {
+	url, err := url.Parse(urlToParse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	parts := strings.Split(url.Hostname(), ".")
+	domain := parts[len(parts)-2] + "." + parts[len(parts)-1]
+	return domain
+}
+
+func Search(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	r.ParseMultipartForm(0)
+	query := r.FormValue("query")
+	books := GetLists(query)
+	component := components.LibgenList(books)
+	component.Render(r.Context(), w)
+}
+
+func Download(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	r.ParseMultipartForm(0)
+	mirrorUrl := r.FormValue("mirror")
+	extension := r.FormValue("extension")
+	downloadUrl := GetDownloadUrl(mirrorUrl)
+	w.Header().Set("Hx-Redirect", downloadUrl)
+
+	component := components.Mirror(mirrorUrl, extension)
+	component.Render(r.Context(), w)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.POST("/search", Search)
+	router.POST("/download", Download)
+	log.Fatal(http.ListenAndServe(":8080", router))
+	fmt.Println("Server started at: 8080")
+
+}
---
+++ scrape.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"libgenscrape/views/components"
+	"net/url"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func EncodeParam(s string) string {
+	return url.QueryEscape(s)
+}
+func GetLists(query string) []components.BookType {
+	c := colly.NewCollector()
+	baseUrl := "https://libgen.is"
+	searchUrl := baseUrl + "/search.php?req=" + EncodeParam(query)
+	var books []components.BookType
+	c.OnHTML("table.c tbody", func(e *colly.HTMLElement) {
+		// Extract data from HTML elements
+		idCol := 0
+		// sizeCol := 0
+		extensionCol := 0
+		mirrorCol1 := 0
+		mirrorCol2 := 0
+		titleCol := 0
+		e.ForEach("tr", func(rowNum int, row *colly.HTMLElement) {
+			var book components.BookType
+			var mirrors []string
+			md5 := ""
+			row.ForEach("td", func(colNum int, col *colly.HTMLElement) {
+				text := col.Text
+				if rowNum == 0 {
+
+					switch text {
+					case "ID":
+						idCol = colNum
+					case "Extension":
+						extensionCol = colNum
+					case "Mirrors":
+						mirrorCol1 = colNum
+						mirrorCol2 = colNum + 1
+					case "Title":
+						titleCol = colNum
+					}
+				} else {
+					switch colNum {
+					case idCol:
+						book.ID = text
+					case extensionCol:
+						book.Extension = text
+					case mirrorCol1:
+						mirrors = append(mirrors, col.ChildAttr("a", "href"))
+					case mirrorCol2:
+						mirrors = append(mirrors, col.ChildAttr("a", "href"))
+					case titleCol:
+						book.BookName = text
+						md5 = strings.Split(col.ChildAttr("a[id]", "href"), "=")[1]
+					}
+				}
+
+			})
+			if rowNum != 0 {
+				coverFolder, err := strconv.ParseInt(book.ID, 10, 32)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if coverFolder < 1000 {
+					coverFolder = 0
+				} else {
+					coverFolder = coverFolder / 1000 * 1000
+				}
+				book.ImgUrl = baseUrl + "/covers/" + strconv.Itoa(int(coverFolder)) + "/" + strings.ToLower(md5)
+				book.Mirrors = mirrors
+				books = append(books, book)
+			}
+		})
+	})
+	err := c.Visit(searchUrl)
+	if err != nil {
+		log.Info(err)
+	}
+
+	return books
+}
+
+func GetDownloadUrl(visitUrl string) string {
+	c := colly.NewCollector()
+	website := GetDomain(visitUrl)
+
+	var downloadUrl string
+
+	selector := "#download"
+
+	if website == "libgen.li" {
+		selector = "#main"
+	}
+
+	c.OnHTML(selector, func(e *colly.HTMLElement) {
+		downloadUrl = e.ChildAttr("a", "href")
+	})
+	err := c.Visit(visitUrl)
+	if err != nil {
+		log.Info(err)
+	}
+	if website == "libgen.li" {
+		downloadUrl = "http://libgen.li/" + downloadUrl
+	}
+	return strings.TrimSpace(downloadUrl)
+}
